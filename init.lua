c3e9cb0703ce56b6f9004918f671496f3d898721
Lunaraa <todorov_spatial_1@abv.bg>
2025-06-13 17:35:23 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local Classes = loadstring(game:HttpGet(https://github.com/PureLunaraa/get-property-of-instance/new/main/Classes.lua))
+local Classes = loadstring(game:HttpGet("https://github.com/PureLunaraa/get-property-of-instance/new/main/Classes.lua"))()
 
 return function(ClassName: string)
 	return Classes[ClassName]
